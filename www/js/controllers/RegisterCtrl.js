1bf44762c8ebacb74426710c3b6cb6d983f5b402
Xhueloe <dorian.grolaux@gmail.com>
2015-09-17 21:06:14 +0200

    Work in progress (menu now available)

--- www/js/controllers/RegisterCtrl.js
+++ www/js/controllers/RegisterCtrl.js
@@ -1,6 +1,12 @@
 angular.module('App')
         .controller('RegisterCtrl', function ($scope, API) {
           $scope.register = function(){
-            
+            if($scope.fname && $scope.lname && $scope.alias && $scope.password){
+              API.register($scope.fname, $scope.lname, $scope.alias, $scope.password, function(reponse){
+                console.log("account created");
+              });
+            } else {
+              alert('Please make sure you filled all fields in');
+            }
           };
         });
