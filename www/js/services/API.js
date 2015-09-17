2a945170d3a1cf4a1a00a9bb3800b23225060d91
Xhueloe <dorian.grolaux@gmail.com>
2015-09-17 21:06:23 +0200

    Work in progress (menu now available)

--- www/js/services/API.js
+++ www/js/services/API.js
@@ -1,5 +1,5 @@
 angular.module('App')
-        .service('API', function ($http) {
+        .service('API', function ($http, $ionicLoading) {
           var SERVER_IP = 'http://192.168.1.30';
           var SERVER_PORT = '50505';
           var baseUrl = SERVER_IP + ':' + SERVER_PORT;
@@ -51,10 +51,30 @@ angular.module('App')
           this.startSitting = function (callback) {
             $http.get(baseUrl + '/client/arrive')
                     .success(function (response) {
-                      
+
                     })
                     .error(function (err) {
                       callback(false, err);
+                    });
+          };
+
+          this.register = function (fname, lname, alias, password, callback) {
+            $ionicLoading.show({
+              template: '<ion-spinner icon="lines"></ion-spinner>'
+            });
+            $http.post(baseUrl + '/client/register')
+                    .success(function () {
+                      $ionicLoading.show({
+                        template: 'Your account has been successfully created <br />' +
+                                'You can now log in with your credentials',
+                        delay: 2000
+                      });
+                    })
+                    .error(function () {
+                      $ionicLoading.show({
+                        template: 'There was an error and your account couldn\'t be created',
+                        delay: 2000
+                      });
                     });
           };
         });
